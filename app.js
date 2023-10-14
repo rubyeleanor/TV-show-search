75d365da5f20de1136af760abe380da0486700b3
rubyhay <115424833+rubyeleanor@users.noreply.github.com>
2023-10-14 17:55:18 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('#searchForm');
+const imgContainer = document.querySelector('#imgContainer');
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
+    makeImages(res.data)
+    form.elements.query.value = '';
+
+
+})
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            imgContainer.append(img);
+        }
+
+
+    }
+}
+
+const clearImages = () => {
+    imgContainer.innerHTML = '';
+}
+
+form.addEventListener('input', clearImages);
